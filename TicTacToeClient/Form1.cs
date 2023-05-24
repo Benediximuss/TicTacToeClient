3d02345f5e84a99efb625df3cca8eaa53121f99a
Benediximuss <ugurpasa699@gmail.com>
2023-05-24 23:29:56 +0300

    commit 3

--- TicTacToeClient/Form1.cs
+++ TicTacToeClient/Form1.cs
@@ -13,10 +13,11 @@ namespace TicTacToeClient
         // Game components
         string username;
 
-        // Network components
+        // Network components comoon
         Socket clientSocket;
         bool isJoined = false;
 
+        // Constructor
         public Form1()
         {
             /*To access UI elements in multi-thread level*/
@@ -27,10 +28,16 @@ namespace TicTacToeClient
 
             InitializeComponent();
 
+
+            // Other initializations
+            setInfoText("init");
             initButtonsMatrix();
             DisableBlueHighlight();
         }
 
+
+
+        // Buttons
         private void btn_Connect_Click(object sender, EventArgs e)
         {
 
@@ -44,13 +51,14 @@ namespace TicTacToeClient
                     clientSocket.Connect(textBox_IP.Text, port); // Tries to connect, if cannot throw exception
 
                     toggleComponentsEnabled(false, label_IP, label_Port, textBox_IP, textBox_Port, btn_Connect);
+                    setInfoText("connected");
 
                     Thread recieveThread = new Thread(serverListener); // Start Recieve function to recieve message from server
                     recieveThread.Start();
                 }
                 catch (Exception exc)
                 {
-                    richTextBox_Log.AppendText(" Couldn't connect to the server!\n");
+                    richTextBox_Log.AppendText("Couldn't connect to the server!\n");
                 }
             }
             else
@@ -67,11 +75,11 @@ namespace TicTacToeClient
             Byte[] buffer_send = Encoding.Default.GetBytes("join:" + username);
             try
             {
-                clientSocket.Send(buffer_send); // Sending username for server to check
+                clientSocket.Send(buffer_send); // Sending username for server to be checked
 
-                toggleComponentsEnabled(false, label_Name, textBox_Name, btn_Join, btn_Dissconnect);
+                toggleComponentsEnabled(false, label_Name, textBox_Name, btn_Join, btn_Disconnect);
 
-                richTextBox_Log.AppendText("Waiting for response...\n");
+                setInfoText("waiting");
             }
             catch
             {
@@ -80,13 +88,51 @@ namespace TicTacToeClient
 
         }
 
-        private void btn_Dissconnect_Click(object sender, EventArgs e)
+        private void btn_Disconnect_Click(object sender, EventArgs e)
         {
-            disconnectServer();
-            toggleComponentsEnabled(true, btn_Join, textBox_Name);
-            toggleComponentsEnabled(false, btn_Dissconnect);
+            if (isJoined) // leave room
+            {
+                disconnectServer();
+                toggleComponentsEnabled(true, btn_Join, textBox_Name, btn_Disconnect);
+                toggleComponentsEnabled(false, btn_Queue);
+                btn_Disconnect.Text = "Disconnect";
+                isJoined = false;
+            }
+            else // disconnect server
+            {
+                toggleComponentsEnabled(true, label_IP, label_Port, textBox_IP, textBox_Port, btn_Connect);
+                toggleComponentsEnabled(false, textBox_Name, btn_Join, btn_Disconnect);
+
+                textBox_Name.Clear();
+
+                clientSocket.Close();
+                clientSocket.Dispose();
+
+            }
         }
 
+        private void btn_Queue_Click(object sender, EventArgs e)
+        {
+            string msg = $"queue:{username}";
+            Byte[] buffer_send = Encoding.Default.GetBytes(msg);
+
+            try
+            {
+                clientSocket.Send(buffer_send);
+                richTextBox_Log.AppendText("You've sent request!\n");
+            }
+            catch
+            {
+                richTextBox_Log.AppendText("Coouldn't send queue request, try again!\n");
+            }
+        }
+
+        private void btn_clearLog_Click(object sender, EventArgs e)
+        {
+            richTextBox_Log.Clear();
+        }
+
+        // Connection
         private void disconnectServer()
         {
             string msg = $"leave:{username}";
@@ -107,44 +153,49 @@ namespace TicTacToeClient
         {
             while (clientSocket.Connected)
             {
+                try
+                {
                     string[] token = receiveResponse();
 
-                    if (token[0] == "200")
+                    if (token[0] == "200") // Connected successfully
                     {
                         richTextBox_Log.AppendText("You have successfully connected to the server!\n");
 
-                        toggleComponentsEnabled(true, label_Name, textBox_Name, btn_Join, btn_Dissconnect);
+                        toggleComponentsEnabled(true, label_Name, textBox_Name, btn_Join, btn_Disconnect);
                     }
-                    else if (token[0] == "400")
+                    else if (token[0] == "400") // Server full
                     {
                         richTextBox_Log.AppendText("Couldn't connect to the server, it is full now!\n");
 
                         toggleComponentsEnabled(true, label_IP, label_Port, textBox_IP, textBox_Port, btn_Connect);
 
-                        //Byte[] buffer_send = Encoding.Default.GetBytes("401:Ending connection!");
-                        //clientSocket.Send(buffer_send);
                         clientSocket.Close();
                     }
-                    else if (token[0] == "201") // username approved, joined successfully
+
+                    else if (token[0] == "201") // Username approved, joined successfully
                     {
                         isJoined = true;
                         richTextBox_Log.AppendText(token[1]);
 
-                        toggleComponentsEnabled(true, btn_Queue, btn_Dissconnect);
+                        toggleComponentsEnabled(true, btn_Queue, btn_Disconnect);
+                        setInfoText("joined");
+                        btn_Disconnect.Text = "Leave";
                     }
-                    else if (token[0] == "401")
+                    else if (token[0] == "401") // Invalid username
                     {
                         richTextBox_Log.AppendText("Please try another username!\n");
 
-                        toggleComponentsEnabled(true, label_Name, textBox_Name, btn_Join, btn_Dissconnect);
-
+                        toggleComponentsEnabled(true, label_Name, textBox_Name, btn_Join, btn_Disconnect);
+                        setInfoText("connected");
                         username = "";
                     }
+
                     else if (token[0] == "info")
                     {
                         richTextBox_Log.AppendText(token[1]);
                     }
-             
+                }
+                catch { }
             }
         }
 
@@ -157,29 +208,7 @@ namespace TicTacToeClient
             return token;
         }
 
-        private void initButtonsMatrix()
-        {
-            List<Button> row;
-
-            row = new List<Button>();
-            row.Add(btn_00);
-            row.Add(btn_01);
-            row.Add(btn_02);
-            buttonsMatrix.Add(row);
-
-            row = new List<Button>();
-            row.Add(btn_10);
-            row.Add(btn_11);
-            row.Add(btn_12);
-            buttonsMatrix.Add(row);
-
-            row = new List<Button>();
-            row.Add(btn_20);
-            row.Add(btn_21);
-            row.Add(btn_22);
-            buttonsMatrix.Add(row);
-        }
-
+        // Component Functions 
         void toggleComponentsEnabled(bool state, params Control[] components)
         {
             foreach (Control component in components)
@@ -199,17 +228,20 @@ namespace TicTacToeClient
             }
         }
 
-        private void buttonPlayed(Button clickedButton)
+        private void setInfoText(string state)
         {
-
+            if (state == "init")
+                label_Notification.Text = "Enter an IP address and a Port Number to connect a server!";
+            else if (state == "connected")
+                label_Notification.Text = "Enter a username to join the game room!";
+            else if (state == "joined")
+                label_Notification.Text = "Join the game queue to play a game!";
+            else if (state == "waiting")
+                label_Notification.Text = "Waiting for response...";
         }
 
 
-        private void resetAll()
-        {
-
-        }
-
+        // XOX Buttons
         private void btn_00_Click(object sender, EventArgs e)
         {
             buttonPlayed((Button)sender);
@@ -255,6 +287,39 @@ namespace TicTacToeClient
             buttonPlayed((Button)sender);
         }
 
+        private void buttonPlayed(Button clickedButton)
+        {
+
+        }
+        private void resetAll()
+        {
+
+        }
+
+        // Initialization
+        private void initButtonsMatrix()
+        {
+            List<Button> row;
+
+            row = new List<Button>();
+            row.Add(btn_00);
+            row.Add(btn_01);
+            row.Add(btn_02);
+            buttonsMatrix.Add(row);
+
+            row = new List<Button>();
+            row.Add(btn_10);
+            row.Add(btn_11);
+            row.Add(btn_12);
+            buttonsMatrix.Add(row);
+
+            row = new List<Button>();
+            row.Add(btn_20);
+            row.Add(btn_21);
+            row.Add(btn_22);
+            buttonsMatrix.Add(row);
+        }
+
         // Disables the blue focus highlight surrounding the buttons, for all buttons
         private void DisableBlueHighlight()
         {
@@ -263,6 +328,7 @@ namespace TicTacToeClient
                 button.TabStop = false;
             }
         }
+
 
         private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -276,5 +342,6 @@ namespace TicTacToeClient
             Environment.Exit(0);
         }
 
+  
     }
 }
